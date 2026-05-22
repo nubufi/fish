aa7da9817625b980c3585fdf34de7e455c2b4381
numanburakfidan <numanburakfidan@yandex.com>
2026-05-22 14:28:19 +0300

    update

--- config.fish
+++ config.fish
@@ -36,23 +36,15 @@ if test -d ~/Applications/depot_tools
     end
 end
 
-
-
-zoxide init fish | source
-
-# Define an fzf function to search projects
-function fzf-search-projects
-    set dir (find ~/DiskD -type d | fzf)
-    if test -n "$dir"
-        cd "$dir"
-    end
+if string match vscode $TERM_PROGRAM > /dev/null
+  source (code --locate-shell-integration-path fish)
+  function fish_prompt
+    printf '> '
+    __vsc_fish_cmd_start
+  end
 end
 
-# Bind fzf-search-projects to Ctrl-F
-bind \cf fzf-search-projects
-## Advanced command-not-found hook
-source /usr/share/doc/find-the-command/ftc.fish
-
+zoxide init fish | source
 
 ## Functions
 # Functions needed for !! and !$ https://github.com/oh-my-fish/plugin-bang-bang
@@ -104,10 +96,13 @@ function copy
     end
 end
 
-# Cleanup local orphaned packages
-function cleanup
-    while pacman -Qdtq
-        sudo pacman -R (pacman -Qdtq)
+function oclaude
+    if count $argv > /dev/null
+        # If an argument is provided, use the first one
+        ollama launch claude --model $argv[1]
+    else
+        # If no argument, use the default
+        ollama launch claude --model glm-5.1:cloud
     end
 end
 
@@ -122,9 +117,6 @@ alias l. 'eza -ald --color=always --group-directories-first --icons .*' # show o
 
 # Replace some more things with better alternatives
 alias cat 'bat --style header --style snip --style changes --style header'
-if not test -x /usr/bin/yay; and test -x /usr/bin/paru
-    alias yay 'paru'
-end
 
 
 # Common use
@@ -133,10 +125,7 @@ alias ... 'cd ../..'
 alias .... 'cd ../../..'
 alias ..... 'cd ../../../..'
 alias ...... 'cd ../../../../..'
-alias big 'expac -H M "%m\t%n" | sort -h | nl'     # Sort installed packages according to size in MB (expac must be installed)
 alias dir 'dir --color=auto'
-alias fixpacman 'sudo rm /var/lib/pacman/db.lck'
-alias gitpkg 'pacman -Q | grep -i "\-git" | wc -l' # List amount of -git packages
 alias grep 'ugrep --color=auto'
 alias egrep 'ugrep -E --color=auto'
 alias fgrep 'ugrep -F --color=auto'
@@ -145,26 +134,16 @@ alias hw 'hwinfo --short'                          # Hardware Info
 alias ip 'ip -color'
 alias psmem 'ps auxf | sort -nr -k 4'
 alias psmem10 'ps auxf | sort -nr -k 4 | head -10'
-alias rmpkg 'sudo pacman -Rdd'
 alias tarnow 'tar -acf '
 alias untar 'tar -zxvf '
-alias upd '/usr/bin/garuda-update'
 alias vdir 'vdir --color=auto'
 alias wget 'wget -c '
-
-# Get fastest mirrors
-alias mirror 'sudo reflector -f 30 -l 30 --number 10 --verbose --save /etc/pacman.d/mirrorlist'
-alias mirrora 'sudo reflector --latest 50 --number 20 --sort age --save /etc/pacman.d/mirrorlist'
-alias mirrord 'sudo reflector --latest 50 --number 20 --sort delay --save /etc/pacman.d/mirrorlist'
-alias mirrors 'sudo reflector --latest 50 --number 20 --sort score --save /etc/pacman.d/mirrorlist'
-
-# Help people new to Arch
-alias apt 'man pacman'
-alias apt-get 'man pacman'
-alias please 'sudo'
-alias tb 'nc termbin.com 9999'
-alias helpme 'echo "To print basic information about a command use tldr <command>"'
-alias pacdiff 'sudo -H DIFFPROG=meld pacdiff'
+alias genie 'cd ~/projects/epam/genieai/genieai/'
+alias ssd 'cd /Volumes/HP-P900'
+alias projects 'cd ~/projects/'
+alias update 'brew update && brew upgrade && brew cleanup && export MAS_NO_AUTO_INDEX=1 && mas upgrade'
+alias n8n 'cd ~/projects/n8n/n8n-compose && docker-compose up -d'
+alias n8n-down 'cd ~/projects/n8n/n8n-compose && docker-compose down'
 
 # Get the error messages from journalctl
 alias jctl 'journalctl -p 3 -xb'
@@ -177,12 +156,19 @@ if status --is-interactive && type -q fastfetch
    fastfetch --config neofetch.jsonc
 end
 
-set -Ux fish_user_paths /home/nubufi/DiskD/sonar/sonar-scanner/bin/ $fish_user_paths
-
 ## Starship prompt
-if status --is-interactive
-   source ("/usr/bin/starship" init fish --print-full-init | psub)
-end
+starship init fish | source
+set -U fish_user_paths /opt/homebrew/bin $fish_user_paths
+
 
 # Added by LM Studio CLI (lms)
-set -gx PATH $PATH /home/nubufi/.cache/lm-studio/bin
+set -gx PATH $PATH /Users/numanburakfidan/.lmstudio/bin
+
+# Added by LM Studio CLI (lms)
+set -gx PATH $PATH /Users/epam/.lmstudio/bin
+
+# Added by Windsurf
+fish_add_path /Users/epam/.codeium/windsurf/bin
+
+# Added by Antigravity
+fish_add_path /Users/epam/.antigravity/antigravity/bin
